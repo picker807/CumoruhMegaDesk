5ace95310290ec60eb365c0a18e632b917ceb003
picker807 <60443210+picker807@users.noreply.github.com>
2023-10-09 22:07:32 -0400

    View quotes functionality
    Added view of all quotes when View Quotes button is clicked.

--- ViewAllQuotes.cs
+++ ViewAllQuotes.cs
@@ -13,11 +13,70 @@ namespace MegaDesk_Picker
     public partial class ViewAllQuotes : Form
     {
         private MainMenu menu;
+        private BindingSource bindingSource = new BindingSource();
+        private FileHandler fileHandler = new FileHandler();
         public ViewAllQuotes(MainMenu menu)
         {
             InitializeComponent();
             this.menu = menu;
+            LoadAllQuotes();
         }
+
+        private void LoadAllQuotes()
+        {
+            try
+            {
+                List<DeskQuote> quotes = fileHandler.LoadQuotes();
+              
+                allQuotesGrid.AutoGenerateColumns = false;
+                allQuotesGrid.RowHeadersVisible = false;
+                allQuotesGrid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                allQuotesGrid.RowsDefaultCellStyle.Font = new Font("Arial", 11, FontStyle.Regular);
+                allQuotesGrid.AlternatingRowsDefaultCellStyle.Font = new Font("Arial", 11, FontStyle.Regular);
+
+
+
+                foreach (var quote in quotes)
+                {
+                    // Convert integer properties to strings
+                    string widthStr = quote.Desk.Width.ToString();
+                    string depthStr = quote.Desk.Depth.ToString();
+                    string numberOfDrawersStr = quote.Desk.NumberOfDrawers.ToString();
+                    string desktopMaterialStr = quote.Desk.DesktopMaterial.ToString();
+                    string rushOrderDaysStr = quote.Desk.RushOrderDays.ToString() + " days";
+                    if (quote.Desk.RushOrderDays == 14)
+                    {
+                        rushOrderDaysStr += " (no rush)";
+                    }
+                    
+
+                    // Add string values to DataGridView
+                    allQuotesGrid.Rows.Add(
+                        quote.CustomerName,
+                        quote.Date.ToShortDateString(),
+                        quote.TotalPrice.ToString("C"),
+                        $"{widthStr} in",
+                        $"{depthStr} in",
+                        numberOfDrawersStr,
+                        desktopMaterialStr,
+                        rushOrderDaysStr
+                    );
+                }
+
+                allQuotesGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                allQuotesGrid.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+
+                //allQuotesGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                //allQuotesGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
 
         private void backButton_Click(object sender, EventArgs e)
         {

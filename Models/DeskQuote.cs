3e57b8763bc8ce76deb97e259ba720431099db4b
marvellouschirunga <marvellouschirunga@gmail.com>
2023-10-11 03:43:27 +0200

    Added GetRushOrder method as well as rushOrderPrices.txt

--- Models/DeskQuote.cs
+++ Models/DeskQuote.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace MegaDesk_Picker
         private double RushOrderCost {  get; set; }
         [JsonProperty]
         internal double TotalPrice { get; set; }
+        private double[,] rushOrderPrices;
 
         public DeskQuote(string customerName, int width, int depth, int numberOfDrawers, DesktopMaterial desktopMaterial, int rushOrderDays)
         {
@@ -80,7 +82,7 @@ namespace MegaDesk_Picker
             return (double)Desk.DesktopMaterial;
         }
 
-        private double CalcRushOrderCost() 
+        private double CalcRushOrderCost()
         {
             double sqInch = Desk.Width * Desk.Depth;
             int rush = Desk.RushOrderDays;
@@ -91,6 +93,9 @@ namespace MegaDesk_Picker
                 return 0;
             }
 
+            // Call GetRushOrder to populate rushOrderPrices array
+            GetRushOrder();
+
             // Headers help to easily find the coordinates of the priceChart
             int[] rushOrderHeader = { 3, 5, 7 };
             int[] deskSizeHeader = { 1000, 2000, 4610 }; //4608 is the max area given the maximum values of width and depth.
@@ -99,17 +104,65 @@ namespace MegaDesk_Picker
             int indexSize = Array.FindIndex(deskSizeHeader, header => sqInch < header);
             // Find the index that matches the value of rush days
             int indexRush = Array.IndexOf(rushOrderHeader, rush);
-            
-            // Chart contains grid of prices for area vs rush days
-            double[,] priceChart = new double[,]
+
+            // Check if rushOrderPrices is null, indicating an error in reading the file
+            if (rushOrderPrices == null)
             {
-                {60, 70, 80 },
-                {40, 50, 60 },
-                {30, 35, 40 }
-            };
-            // Return the value corresponding to the indexSize and indexRush coordinates
-            return priceChart[indexSize, indexRush];
+                // Handle the error, for example, by returning a default value or showing an error message.
+                Console.WriteLine("Error: Unable to retrieve rush order prices.");
+                return 0; // Default value, or handle the error as needed.
+            }
+
+            // Calculate the rush order cost using rushOrderPrices array
+            double rushOrderCost = rushOrderPrices[indexSize, indexRush];
+
+            return rushOrderCost;
         }
+
+        // New method to handle rush order prices
+        public void GetRushOrder()
+        {
+            try
+            {
+                string filePath = "Resources/rushOrderPrices.txt";
+                string[] lines = File.ReadAllLines(filePath);
+
+                // Initialize the existing class-level 2D array with three rows and three columns
+                rushOrderPrices = new double[3, 3];
+
+                int row = 0;
+                int col = 0;
+
+                foreach (string line in lines)
+                {
+                    // Parse the price from each line and add it to the array
+                    if (row < 3 && col < 3)
+                    {
+                        if (double.TryParse(line, out double price))
+                        {
+                            rushOrderPrices[row, col] = price;
+                            col++;
+                        }
+                    }
+
+                    if (col >= 3)
+                    {
+                        col = 0;
+                        row++;
+                    }
+
+                    if (row >= 3)
+                    {
+                        break; // We have filled the entire 3x3 array
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while reading rush order prices: {ex.Message}");
+            }
+        }
+
 
         private double CalcTotalPrice()
         {

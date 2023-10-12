35ccd88bbdfcabc72c75eb97dae2bbc074539537
picker807 <60443210+picker807@users.noreply.github.com>
2023-10-12 16:41:36 -0400

    Aaron1 (#1)
    * View quotes functionality
    
    Added view of all quotes when View Quotes button is clicked.
    
    * Update ViewAllQuotes.cs
    
    * Update ViewAllQuotes.cs

--- ViewAllQuotes.Designer.cs
+++ ViewAllQuotes.Designer.cs
@@ -28,28 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.backButton = new System.Windows.Forms.Button();
+            this.allQuotesGrid = new System.Windows.Forms.DataGridView();
+            this.CustomerName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TotalPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Width = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Depth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NumberOfDrawers = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DesktopMaterial = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RushOrderDays = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.allQuotesGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // backButton
             // 
-            this.backButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.backButton.Location = new System.Drawing.Point(35, 28);
+            this.backButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backButton.Location = new System.Drawing.Point(18, 15);
+            this.backButton.Margin = new System.Windows.Forms.Padding(2);
             this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(148, 52);
+            this.backButton.Size = new System.Drawing.Size(116, 27);
             this.backButton.TabIndex = 0;
-            this.backButton.Text = "Back";
+            this.backButton.Text = "Main Menu";
             this.backButton.UseVisualStyleBackColor = true;
             this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
+            // allQuotesGrid
+            // 
+            this.allQuotesGrid.AllowDrop = true;
+            this.allQuotesGrid.AllowUserToDeleteRows = false;
+            this.allQuotesGrid.AllowUserToOrderColumns = true;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.BurlyWood;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.allQuotesGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.allQuotesGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.allQuotesGrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.allQuotesGrid.CausesValidation = false;
+            this.allQuotesGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleVertical;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.allQuotesGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.allQuotesGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.allQuotesGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CustomerName,
+            this.Date,
+            this.TotalPrice,
+            this.Width,
+            this.Depth,
+            this.NumberOfDrawers,
+            this.DesktopMaterial,
+            this.RushOrderDays});
+            this.allQuotesGrid.Location = new System.Drawing.Point(11, 59);
+            this.allQuotesGrid.Margin = new System.Windows.Forms.Padding(2);
+            this.allQuotesGrid.Name = "allQuotesGrid";
+            this.allQuotesGrid.ReadOnly = true;
+            this.allQuotesGrid.RowHeadersWidth = 82;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.BurlyWood;
+            this.allQuotesGrid.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.allQuotesGrid.RowTemplate.Height = 33;
+            this.allQuotesGrid.RowTemplate.ReadOnly = true;
+            this.allQuotesGrid.Size = new System.Drawing.Size(941, 391);
+            this.allQuotesGrid.TabIndex = 1;
+            // 
+            // CustomerName
+            // 
+            this.CustomerName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.CustomerName.HeaderText = "Name";
+            this.CustomerName.MinimumWidth = 10;
+            this.CustomerName.Name = "CustomerName";
+            this.CustomerName.ReadOnly = true;
+            this.CustomerName.Width = 200;
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Date";
+            this.Date.MinimumWidth = 10;
+            this.Date.Name = "Date";
+            this.Date.ReadOnly = true;
+            // 
+            // TotalPrice
+            // 
+            this.TotalPrice.HeaderText = "Total Price";
+            this.TotalPrice.MinimumWidth = 10;
+            this.TotalPrice.Name = "TotalPrice";
+            this.TotalPrice.ReadOnly = true;
+            // 
+            // Width
+            // 
+            this.Width.HeaderText = "Width";
+            this.Width.MinimumWidth = 10;
+            this.Width.Name = "Width";
+            this.Width.ReadOnly = true;
+            // 
+            // Depth
+            // 
+            this.Depth.HeaderText = "Depth";
+            this.Depth.MinimumWidth = 10;
+            this.Depth.Name = "Depth";
+            this.Depth.ReadOnly = true;
+            // 
+            // NumberOfDrawers
+            // 
+            this.NumberOfDrawers.HeaderText = "Drawers";
+            this.NumberOfDrawers.MinimumWidth = 10;
+            this.NumberOfDrawers.Name = "NumberOfDrawers";
+            this.NumberOfDrawers.ReadOnly = true;
+            // 
+            // DesktopMaterial
+            // 
+            this.DesktopMaterial.HeaderText = "Material";
+            this.DesktopMaterial.MinimumWidth = 10;
+            this.DesktopMaterial.Name = "DesktopMaterial";
+            this.DesktopMaterial.ReadOnly = true;
+            // 
+            // RushOrderDays
+            // 
+            this.RushOrderDays.HeaderText = "Rush Order";
+            this.RushOrderDays.MinimumWidth = 10;
+            this.RushOrderDays.Name = "RushOrderDays";
+            this.RushOrderDays.ReadOnly = true;
+            // 
             // ViewAllQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(963, 461);
+            this.ControlBox = false;
+            this.Controls.Add(this.allQuotesGrid);
             this.Controls.Add(this.backButton);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ViewAllQuotes";
-            this.Text = "ViewAllQuotes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "All Quotes";
+            ((System.ComponentModel.ISupportInitialize)(this.allQuotesGrid)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,5 +183,14 @@
         #endregion
 
         private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.DataGridView allQuotesGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CustomerName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TotalPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Width;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Depth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NumberOfDrawers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DesktopMaterial;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RushOrderDays;
     }
 }
--- ViewAllQuotes.cs
+++ ViewAllQuotes.cs
@@ -13,11 +13,67 @@ namespace MegaDesk_Picker
     public partial class ViewAllQuotes : Form
     {
         private MainMenu menu;
+        private BindingSource bindingSource = new BindingSource();
+        private FileHandler fileHandler = new FileHandler();
         public ViewAllQuotes(MainMenu menu)
         {
             InitializeComponent();
             this.menu = menu;
+            LoadAllQuotes();
         }
+
+        private void LoadAllQuotes()
+        {
+            try
+            {
+                List<DeskQuote> quotes = fileHandler.LoadQuotes();
+              
+                allQuotesGrid.AutoGenerateColumns = false;
+                allQuotesGrid.RowHeadersVisible = false;
+                allQuotesGrid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+                allQuotesGrid.RowsDefaultCellStyle.Font = new Font("Arial", 11, FontStyle.Regular);
+                allQuotesGrid.AlternatingRowsDefaultCellStyle.Font = new Font("Arial", 11, FontStyle.Regular);
+                allQuotesGrid.ColumnHeadersDefaultCellStyle.Font = new Font("Arial", 12, FontStyle.Bold);
+                allQuotesGrid.ColumnHeadersDefaultCellStyle.WrapMode = DataGridViewTriState.False;
+
+                foreach (var quote in quotes)
+                {
+                    // Convert integer properties to strings
+                    string widthStr = quote.Desk.Width.ToString();
+                    string depthStr = quote.Desk.Depth.ToString();
+                    string numberOfDrawersStr = quote.Desk.NumberOfDrawers.ToString();
+                    string desktopMaterialStr = quote.Desk.DesktopMaterial.ToString();
+                    string rushOrderDaysStr = quote.Desk.RushOrderDays.ToString() + " days";
+                    if (quote.Desk.RushOrderDays == 14)
+                    {
+                        rushOrderDaysStr += " (no rush)";
+                    }
+                    
+
+                    // Add string values to DataGridView
+                    allQuotesGrid.Rows.Add(
+                        quote.CustomerName,
+                        quote.Date.ToShortDateString(),
+                        quote.TotalPrice.ToString("C0"),
+                        $"{widthStr} in",
+                        $"{depthStr} in",
+                        numberOfDrawersStr,
+                        desktopMaterialStr,
+                        rushOrderDaysStr
+                    );
+                }
+                // Resize columns to accomodate cell contents (min width) and fill the form.
+                allQuotesGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                allQuotesGrid.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
 
         private void backButton_Click(object sender, EventArgs e)
         {
